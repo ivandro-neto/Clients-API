167c9591fae462cc346a415124cccd9adaec4c2e
Ivandro Neto <ivandroneto1@outlook.com>
2023-05-14 14:27:55 +0100

    add

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const start = async () => {
   try {
     const data = {
       database: process.env.DB_DATABASE,
-      host: 'containers-us-west-209.railway.app' | process.env.DB_HOST ,
+      host: "containers-us-west-209.railway.app" || process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
