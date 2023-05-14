768bdc1e387e5717ff642ffd9f878259a130b80e
Ivandro Neto <ivandroneto1@outlook.com>
2023-05-14 20:33:39 +0100

    update

--- index.js
+++ index.js
@@ -9,20 +9,19 @@ app.use(express.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any domain
   res.header(
-    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
 
-app.get('/', (req, res, next) => {
-  res.send('OK')
-  next()
-})
+app.get("/", (req, res, next) => {
+  res.send("OK");
+  next();
+});
 app.use("/api/v1", clientRoute);
 const start = async () => {
   try {
-  
     await app.listen(process.env.PORT, process.env.SERVER);
     console.log("Serving at port" + process.env.PORT);
   } catch (error) {
