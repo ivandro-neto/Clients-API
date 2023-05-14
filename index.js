abbb9e2ea115f244995360124b7f3fa3ad4a6521
Ivandro Neto <ivandroneto1@outlook.com>
2023-05-14 23:41:01 +0100

    add

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 require("dotenv").config();
-const helmet = require("helmet");
-const cors = require("cors");
-const xss = require("xss-clean");
-const rateLimiter = require("express-rate-limit");
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 const express = require("express");
 const { clientRoute } = require("./routes");
 const app = express();
 
-app.set("trust proxy", 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-);
 app.use(express.json());
-app.use(helmet());
 app.use(cors());
-app.use(xss());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any domain
+  res.header(
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res, next) => {
   res.send("OK");
